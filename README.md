f006251801fb5d3cafe2f2e81823d1dedb702f9f
Christopher Wiratman <144767748+CJPortfolio@users.noreply.github.com>
2025-05-25 20:24:03 -0500

    TODO and Randomizer function
    Updated TODO and added int randomizer function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DataAlgorithmVisualization
 Program that visualizes data algorithms and how they work
+
+# TODO
+1. Print data with bar chart(function prob printing the array with characters)
--- src/main/java/com/datavisualizationproject/Main.java
+++ src/main/java/com/datavisualizationproject/Main.java
@@ -1,16 +1,29 @@
 package com.datavisualizationproject;
 
+import java.util.Random;
 
 public class Main 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Quicksort Here I Come!" );
+        
     }
 
     public static void printData()
     {
         
+    }
+
+
+    //Generates random int
+    public static int intRandomizer()
+    {
+        //Creates instant of random class
+        Random r = new Random();
+        //Generates random integer from 0 to 1000
+        int randNumber = r.nextInt(1001);
+
+        return randNumber;
     }
 }
 
