05fbe614c9656fdc6078a2c675c06b05cd49b1cc
Christopher Wiratman <144767748+CJPortfolio@users.noreply.github.com>
2025-05-27 23:51:03 -0500

    Function for data visuals
    Added PrintData function. Need to add array randomizer function to initalize array with randomized numbers

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Program that visualizes data algorithms and how they work
 
 # TODO
 1. Print data with bar chart(function prob printing the array with characters)
+
+# Notes
+*Array length is hardcoded to 50
--- src/main/java/com/datavisualizationproject/Main.java
+++ src/main/java/com/datavisualizationproject/Main.java
@@ -1,30 +1,41 @@
 package com.datavisualizationproject;
 
-import java.util.Random;
-
 public class Main 
 {
     public static void main( String[] args )
     {
-        
+        int[] dataSet = ArrayValueRandomizer();
+        printData(dataSet);
     }
 
-    public static void printData()
+    public static void printData(int[] data)
     {
-        
+        //Outer goes through entire array
+        for (int i = 0; i < data.length; ++i)
+        {
+            //Inner prints amount of characters the element in the array's number is
+            for (int j = 0; j < data[i]; ++j)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        } 
     }
 
 
     //Generates random int
-    public static int intRandomizer()
+    public static int[] ArrayValueRandomizer()
     {
-        //Creates instant of random class
-        Random r = new Random();
-        //Generates random integer from 0 to 1000
-        int randNumber = r.nextInt(1001);
-
-        return randNumber;
+    
+        
+        int dataSetLength = 31;
+        int[] dataSet = new int[dataSetLength];
+ 
+        
+        return dataSet;
     }
+
+
 }
 
 
